7111d280716bfc4b2b83257613d1badce681c3ec
shaikhadil26 <m.adilshaikh26@gmail.com>
2022-11-07 22:50:23 +0530

    final

--- final.py
+++ final.py
@@ -9,10 +9,10 @@ cbot = ChatBot('Baymax')
 
 # Training the chat bot using yml files
 trainer = ChatterBotCorpusTrainer(cbot)
-trainer.train(r'C:\Users\Adil\Desktop\IS\training\conversations.yml',
-              r'C:\Users\Adil\Desktop\IS\training\greetings.yml',
-              r'C:\Users\Adil\Desktop\IS\training\medical.yml',              
-              r'C:\Users\Adil\Desktop\IS\training\trivia.yml'
+trainer.train(r'C:\Users\Adil\Desktop\IS proj\training\conversations.yml',
+              r'C:\Users\Adil\Desktop\IS proj\training\greetings.yml',
+              r'C:\Users\Adil\Desktop\IS proj\training\medical.yml',              
+              r'C:\Users\Adil\Desktop\IS proj\training\trivia.yml'
               )
 
 # Local database of diseases with their symptoms
@@ -47,40 +47,48 @@ def check_list(symptom, diseases_list):
         for j in range(1,5):
             if symptom.lower() == diseases_list[i][j].lower():
                 diseases_list[i][5] += 1
+
     return diseases_list
 
 # Function to get index of the disease with the highest probability(count)
 def disease_index(diseases_list):
+    x = []
     list1 = [0]*len(diseases_list)
+
     for i in range(len(diseases_list)):
-        list1[i] = diseases_list[i][5]        
+        list1[i] = diseases_list[i][5]
+
     maximum = max(list1)
-    index1 = list1.index(maximum)
+    for i in range(len(list1)):
+        if list1[i] == maximum:
+            x.append(i)
+            
     if maximum == 0:
-        return -1
-    return index1
+        return None
+
+    return x
 
 # Function to get index of disease that the user wants to search
 def search_disease(Disease, diseases_list):
     for i in range(len(diseases_list)):
         if Disease.lower() == diseases_list[i][0].lower():
             return i
+
     else:
         return -1
 
 #Function to get a dashed line for aesthetics in terminal
 def line():
     print()
-    for i in range(0, 72):
-        print('-', end= '')
+    print('-'*72)
     print()
 
 #Function to get a starred line for aesthetics in terminal
 def sline():
     print()
-    for i in range(0, 72):
-        print('*', end= '')
+    print('*'*72)
     print()
+    
 
 #Defining a local user database for existing users   
 users_pass = {'abc': '123',
@@ -103,14 +111,16 @@ if ch1 == 'y':
             print('User already exists')
         else:
             break
+
     password = getpass.getpass("enter new password: ")
     users_pass[user_name] = password
+    users_details[user_name] = []
 
     print('\nPlease enter your personal details: ')
     height = input('Enter your height(in inches): ')
     weight = input('Enter your weight(in kgs): ')
-    temp.append([height, weight])
-    users_details[user_name] = temp
+
+    users_details[user_name] += [height, weight]
     print('\n\n\tAccount created\n\tPlease login again\n')
 
 line()
@@ -123,6 +133,7 @@ while count > 0:
     if user in users_pass and users_pass[user] == pass1:
         name = user
         break
+
     else:
         line()
         count -= 1
@@ -137,123 +148,123 @@ print(f"Baymax- Hello there i'm Baymax\nWelcome {name}")
 while True:
     try:
         sline()
-        #Menu for convenience of User
-        ch2 = input(f'''\nWhat would you like to do today
-1. Symptom analysis.
-2. Disease symptoms.
-3. MEDICAL EMERGENCY.
-4. Corona virus test.
-5. Do you want to speak to Baymax?
-6. View/change user details.
-7. Exit.\n''')   
-
         sline()
-        #Swicth case to implement the menu
-        match ch2:
-            case '1':
-                # print('success')
-                list1=[] 
-                line()
-                sym1=input('Baymax- what symptom do u have?\n')
-                disease_list = check_list(sym1, disease_list)
-                list1.append(sym1)
-                while True:
-                    choice=input('Baymax- Do have any other symptoms?\n')
-                    if choice=='yes':
-                        sym=input("Baymax- please enter other symptoms\n")
-                        disease_list = check_list(sym, disease_list)
-                        list1.append(sym)
-                    elif choice=='no':
-                        break
-                    else:
-                        print('Baymax- Please enter a valid input')
-                index1 = disease_index(disease_list)
-                if index1 == -1:
-                    print('Baymax- Disease not available in database checking for symptoms online :(')
-                    symps='+'.join(list1)
-                    print("Baymax- Found details online")
-                    kt.search(symps)
-                else:    
-                    print(f'Baymax- You are most likely suffering from {disease_list[index1][0]}')
+        inp = input(f'{name}- ')
+
+        if 'symptom' in inp.lower():
+            list1=[] 
+            line()
+            sym1=input('Baymax- what symptom do u have?\n')
+            disease_list = check_list(sym1, disease_list)
+            list1.append(sym1)
+
+            while True:
+                choice=input('Baymax- Do have any other symptoms?\n')
+                if choice=='yes':
+                    sym=input("Baymax- please enter other symptoms\n")
+                    disease_list = check_list(sym, disease_list)
+                    list1.append(sym)
+
+                elif choice=='no':
+                    break
+                else:
+                    print('Baymax- Please enter a valid input')
+
+            index_list = disease_index(disease_list)
+
+            if index_list == None:
+                print('Baymax- Disease not available in database checking for symptoms online :(')
+                symps='+'.join(list1)
+                print("Baymax- Found details online")
+                kt.search(symps)
+
+            else: 
+                if len(index_list) == 1:   
+                    print(f'Baymax- You are most likely suffering from {disease_list[index_list[0]][0]}')
                     ch3 = input('Baymax- Do you want to know more about this disease?: ').lower()
                     if ch3 == 'yes':
-                        b = disease_list[index1][0]
+                        b = disease_list[index_list[0]][0]
                         kt.search(b)
-                for i in range(len(disease_list)):
-                    disease_list[i][5] = 0        
-                line()
 
-            case '2':
-                line()
-                a = (input('Baymax- Enter name of disease you want details of: '))
-                index2 = search_disease(a, disease_list)
-                if index2 == -1:
-                    print(f"Baymax- details of {a} not available in existing database :( \nExpanding disease database and Checking disease symptoms online! ")
-                    kt.search(a)
                 else:
-                    print(f"Baymax-Found details of {a}")
-                    print("Baymax-These are the symptoms of the following conditions")
-                    print(f'1. {disease_list[index2][1]} \n2. {disease_list[index2][2]} \n3. {disease_list[index2][3]} \n4. {disease_list[index2][4]} \n')
-                    ch4 = input('Baymax- Do you want to know more about this disease?: ').lower()
-                    if ch4 == 'yes':
-                        kt.search(a)
-                # print(f"Baymax-Found details of {a}")
-                # print("Baymax-These are the symptoms of the following conditions")
-                # kt.search(a)
-                line()
+                    l2 = [0]*len(index_list)
+                    print(f'Baymax- You have symptoms of following diseases: - ')  
+                    for i in range(len(index_list)):
+                        l2[i] = disease_list[index_list[i]][0]
+                        print(f'{i+1}. {disease_list[index_list[i]][0]}')
+                    ch3 = input('Baymax- Do you want to know more about this disease(s)?: ').lower()
+                    if ch3 == 'yes':
+                        for i in range(len(index_list)):
+                            x = l2[i]
+                            kt.search(x)
 
-            case '3':
-                line()
-                c = input('Baymax- Enter your location: ')
-                print("Baymax- Finding nearby hospitals")
-                b = 'hospitals near ' + c
-                kt.search(b)
-                line()
+            for i in range(len(disease_list)):
+                disease_list[i][5] = 0        
+            line()
+            
+        elif 'disease' in inp.lower():
+            line()
+            a = (input('Baymax- Enter name of disease you want details of: '))
+            index2 = search_disease(a, disease_list)
 
-            case '4':
-                line()
-                c = input("Baymax- Enter your location: ")
-                print("Baymax- Found test centres near you")       
-                b = 'covid test centres near- ' + c
-                kt.search(b)
-                line()
+            if index2 == -1:
+                print(f"Baymax- details of {a} not available in existing database :( \nExpanding disease database and Checking disease symptoms online! ")
+                kt.search(a)
 
-            case '5':
-                sline()
-                bot_input = cbot.get_response('hi')
-                print('Baymax-',bot_input)
-                while 1:
-                    a = (input(''))
-                    if a.lower() == "quit" or a.lower() == 'exit':
-                        break
-                    bot_input = cbot.get_response(a)
-                    print('Baymax- ',bot_input)
-                sline()
+            else:
+                print(f"Baymax-Found details of {a}")
+                print("Baymax-These are the symptoms of the following conditions")
+                print(f'1. {disease_list[index2][1]} \n2. {disease_list[index2][2]} \n3. {disease_list[index2][3]} \n4. {disease_list[index2][4]} \n')
+                ch4 = input('Baymax- Do you want to know more about this disease?: ').lower()
+                if ch4 == 'yes':
+                    kt.search(a)
+            line()
+            
+        elif 'emergency' in inp.lower():
+            line()
+            print("Baymax- Enter your location")
+            print("Baymax- Finding nearby hospitals")  
+            c = input('')
+            b = 'hospitals near- ' + c
+            kt.search(b)
+            line()
 
-            case '6':
-                line()
+        elif 'test' in inp.lower() or 'rt-pcr' in inp.lower():
+            line()
+            print("Baymax- Enter your location")
+            c = input('')
+            print("Baymax- Found test centres near you")       
+            b = 'covid test centres near- ' + c
+            kt.search(b)
+            line()
+
+        elif 'setting' in inp.lower():
+            line()
+            print(f'Baymax- Hello {name} \nWhat would you like to do today?')
+            ch5 = input('1. View user details \n2. Change user details \n')
+
+            if ch5 == '1':
                 print(f'{name} details: ')
                 print(f'height: - {users_details[name][0]}')
                 print(f'weight: - {users_details[name][1]}')
-                line()
-                ch4 = input('Baymax- Would you like to make changes: ').lower
-                if ch4 == 'yes':
-                    new_height = input('Enter new height: ')
-                    users_details[name][0] = new_height
-                    new_weight = input('Enter new weight: ')
-                    users_details[name][1] = new_weight
-                    print('\nBaymax- Response recorded!\n')
-                line()
 
-            case '7':
-                print('Baymax- Thank you \nHave a great day :)')
-                sline()
-                break
+            elif ch5 == '2':
+                new_height = input('Enter new height: ')
+                users_details[name][0] = new_height
+                new_weight = input('Enter new weight: ')
+                users_details[name][1] = new_weight
+                
+                print('\nBaymax- Response recorded!\n')
+            line()
 
-            case default:
-                print('Invalid Input!')
+        elif 'exit' in inp.lower() or 'quit'in inp.lower() or 'bye' in inp.lower():
+            print('Baymax- Thank you \nHave a great day :)')
+            sline()
+            exit()
+            
+        else:
+            bot_input = cbot.get_response(inp)
+            print('Baymax-',bot_input)
 
-        # bot_input = cbot.get_response(a)
-        # print('Baymax- ',bot_input)
     except(KeyboardInterrupt,EOFError,SystemExit):
         break
